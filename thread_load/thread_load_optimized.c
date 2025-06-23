a9b4b55f42ab71a7b28d238b3858983e2bb4626f
Mihai IDU <midu@redhat.com>
2025-06-23 15:09:44 +0200

    adding thread sleep

--- thread_load/thread_load_optimized.c
+++ thread_load/thread_load_optimized.c
@@ -6,62 +6,91 @@
 #include <errno.h>
 #include <string.h>
 #include <time.h>
-#include <sched.h>
 
-// Number of threads
-#define NUM_THREADS 64
-#define LOOP_ITERATIONS 100000000
+#define THREADS 64
+#define THREADS_PER_MUTEX 4
+#define MUTEX_COUNT (THREADS / THREADS_PER_MUTEX)
+#define LOOP_COUNT 100000
+#define THREAD_SLEEP_US 100
+#define OPERATIONS 100000
 
-// Spinlock for shared counter
-pthread_spinlock_t spinlock;
+// Replace mutexes with spinlocks
+pthread_spinlock_t spinlocks[MUTEX_COUNT];
+pthread_spinlock_t mt_count;
 
-uint64_t shared_counter = 0;
+int ops = 0;
 
-void *thread_function(void *arg) {
-    int cpu = (int)(intptr_t)arg;
+// Dummy logging function for illustration
+void timestamp_log(const char *fmt, int val) {
+    printf(fmt, val);
+}
 
-    // Pin this thread to its own CPU core
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(cpu, &cpuset);
+void* thread_func(void* arg) {
+    int index = *(int *)arg;
+    free(arg);
 
-    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) != 0) {
-        fprintf(stderr, "Warning: Could not set CPU affinity for thread %d: %s\n", cpu, strerror(errno));
+    pthread_spinlock_t *lock = &spinlocks[index / THREADS_PER_MUTEX];
+
+    while (1) {
+        pthread_spin_lock(lock);
+
+        volatile int temp = 0;
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            temp += i;
+        }
+
+        pthread_spin_unlock(lock);
+
+        if (OPERATIONS > 0) {
+            pthread_spin_lock(&mt_count);
+            if (ops >= OPERATIONS) {
+                timestamp_log("operation finished - %d\n", ops);
+                pthread_spin_unlock(&mt_count);
+                exit(0);
+            }
+            ops++;
+            pthread_spin_unlock(&mt_count);
+        }
+
+        usleep(THREAD_SLEEP_US);
     }
 
-    for (int i = 0; i < LOOP_ITERATIONS; i++) {
-        pthread_spin_lock(&spinlock);
-        shared_counter++;
-        pthread_spin_unlock(&spinlock);
-    }
-
-    pthread_exit(NULL);
+    return NULL;
 }
 
 int main(void) {
-    pthread_t threads[NUM_THREADS];
-    int rc;
+    pthread_t threads[THREADS];
 
-    if (pthread_spin_init(&spinlock, PTHREAD_PROCESS_PRIVATE) != 0) {
-        perror("pthread_spin_init");
-        exit(EXIT_FAILURE);
-    }
-
-    for (intptr_t i = 0; i < NUM_THREADS; i++) {
-        rc = pthread_create(&threads[i], NULL, thread_function, (void *)i);
-        if (rc != 0) {
-            fprintf(stderr, "Error creating thread %ld: %s\n", i, strerror(rc));
+    // Init spinlocks
+    for (int i = 0; i < MUTEX_COUNT; i++) {
+        if (pthread_spin_init(&spinlocks[i], PTHREAD_PROCESS_PRIVATE) != 0) {
+            perror("spinlock init failed");
             exit(EXIT_FAILURE);
         }
     }
 
-    for (int i = 0; i < NUM_THREADS; i++) {
+    if (pthread_spin_init(&mt_count, PTHREAD_PROCESS_PRIVATE) != 0) {
+        perror("spinlock init failed");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < THREADS; i++) {
+        int *arg = malloc(sizeof(int));
+        *arg = i;
+        if (pthread_create(&threads[i], NULL, thread_func, arg) != 0) {
+            perror("thread create failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    pthread_spin_destroy(&spinlock);
-
-    printf("Final counter value: %lu\n", shared_counter);
+    for (int i = 0; i < MUTEX_COUNT; i++) {
+        pthread_spin_destroy(&spinlocks[i]);
+    }
+    pthread_spin_destroy(&mt_count);
 
     return 0;
 }
